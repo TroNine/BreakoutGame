fb9e9b6512838bd7858948e4b57d42cd07cc3af8
TroNine-PC <plume142@gmail.com>
2018-12-17 02:52:33 +0700

    Initial commit

---
+++ app/src/main/java/com/example/tronine_pc/breakoutgame/Paddle.java
@@ -0,0 +1,59 @@
+package com.example.tronine_pc.breakoutgame;
+
+import android.graphics.RectF;
+
+public class Paddle {
+
+    private RectF rect;
+
+    private float length;
+    private float height;
+
+    private float x;
+
+    private float y;
+
+    private float paddleSpeed;
+
+    public final int STOPPED = 0;
+    public final int LEFT = 1;
+    public final int RIGHT = 2;
+
+    private int paddleMoving = STOPPED;
+
+    public Paddle(int screenX, int screenY){
+        length = 300;
+        height = 40;
+
+        x = screenX / 2;
+        y = screenY - 100;
+
+        rect = new RectF(x, y, x + length, y + height);
+
+        paddleSpeed = 700;
+    }
+
+
+    public RectF getRect(){
+        return rect;
+    }
+
+    public void setMovementState(int state){
+        paddleMoving = state;
+    }
+
+    public void update(long fps){
+        if(paddleMoving == LEFT){
+            x = x - paddleSpeed / fps;
+        }
+
+        if(paddleMoving == RIGHT){
+            x = x + paddleSpeed / fps;
+        }
+
+        rect.left = x;
+        rect.right = x + length;
+    }
+
+}
+
